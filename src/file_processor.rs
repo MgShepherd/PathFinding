3f5032430d4d930e71da2fad53526ec2327487d8
MgShepherd <michaelshep1504@gmail.com>
2025-05-25 21:42:46 +0100

    Added Custom error type to improved error handling

--- src/file_processor.rs
+++ src/file_processor.rs
@@ -1,10 +1,34 @@
 use std::fs;
 use std::io;
 
-pub fn process_file(file_path: &str) -> Result<(), io::Error> {
+#[derive(Debug)]
+pub enum ProcessFileError {
+    FileReadError(String, io::Error),
+    MazeGenerationError
+}
+
+impl std::fmt::Display for ProcessFileError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+      match self {
+          ProcessFileError::FileReadError(name, e) => write!(f, "Problem reading {}: {}", name, e),
+          ProcessFileError::MazeGenerationError => write!(f, "Problem generating valid maze from input file")
+      }
+  }
+}
+
+impl std::error::Error for ProcessFileError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ProcessFileError::FileReadError(_, e) => Some(e),
+            ProcessFileError::MazeGenerationError => None
+        }
+    }
+}
+
+pub fn process_file(file_path: &str) -> Result<(), ProcessFileError> {
     println!("Processing file");
 
-    let file_contents = fs::read_to_string(file_path)?;
+    let file_contents = fs::read_to_string(file_path).map_err(|e| ProcessFileError::FileReadError(file_path.to_owned(), e))?;
     println!("Contents of file were: {}", file_contents);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use file_processor::ProcessFileError;
+
 pub mod file_processor;
 
-fn main() {
-    let file_name = "examples/maze.txt";
-    if let Some(file_error) = file_processor::process_file(file_name).err() {
-        println!("Error occurred processing file {}: {}", file_name, file_error);
-    }
+fn main() -> Result<(), ProcessFileError> {
+    let file_name = "examples/maze1.txt";
+    file_processor::process_file(file_name)
 }
